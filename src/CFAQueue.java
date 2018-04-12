1a0bc43e49fda36e4f918f20d4ac644a0a49d34c
Zachary Reese <31252377+zr00555@users.noreply.github.com>
2018-04-12 17:03:44 -0400

    Queue

---
+++ src/CFAQueue.java
@@ -0,0 +1,19 @@
+package FinalProject;
+
+public class CFAQueue {
+	
+	 public static void main(String[] args) 
+	    {
+	        Queue q= new Queue();
+	        q.enqueue("Jesus");
+	        q.enqueue("Mark");
+	        q.enqueue("Bob");
+	        q.enqueue("Pat");
+	        q.enqueue("Hello");
+	         
+	        System.out.println("Dequeued item is "+ q.dequeue().key);
+	        System.out.println("Dequeued item is "+ q.dequeue().key);
+	        System.out.println("Dequeued item is "+ q.dequeue().key);
+	        System.out.println("Dequeued item is "+ q.dequeue().key);
+}
+}
---
+++ src/Node.java
@@ -0,0 +1,12 @@
+package FinalProject;
+
+public class Node {
+	    String key;
+	    Node next;
+	     
+	    // constructor to create a new linked list node
+	    public Node(String key) {
+	        this.key = key;
+	        this.next = null;
+	    }
+	}
